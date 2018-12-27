98bfa0beb84bcdbd595cfa811242a05e59747b27
MingNia <294583492huang@gmail.com>
2018-12-27 10:57:28 +0800

    用户模块登录/注册

---
+++ duangduangwang/Models/Mapper/UserMapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace duangduangwang.Models.Mapper
+{
+    public class UserMapper
+    {
+        private DataClasses1DataContext db = new DataClasses1DataContext();
+        public List<Customer> Alllist()
+        {
+            var results = from r in db.Customer
+                          select r;
+            List<Customer> templist = results.ToList();
+            return templist;
+        }
+        public void Insert(Customer customer)
+        {
+            db.Customer.InsertOnSubmit(customer);
+            db.SubmitChanges();
+        }
+    }
+}
