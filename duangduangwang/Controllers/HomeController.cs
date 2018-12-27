bfb5a3a9eaa3161a9f50db6037fdb3fd9e50d7bb
MingNia <294583492huang@gmail.com>
2018-12-27 16:31:32 +0800

    用户管理

--- duangduangwang/Controllers/HomeController.cs
+++ duangduangwang/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace duangduangwang.Controllers
     {
         private BookMapper bookMapper = new BookMapper();
         private OrderMapper orderMapper = new OrderMapper();
+        private UserMapper userMapper = new UserMapper();
 
         //跳转到主页
         public ActionResult Index()
@@ -33,6 +34,13 @@ namespace duangduangwang.Controllers
             return View();
         }
 
+        //跳转到用户管理页面/显示全部用户信息
+        public ActionResult ManageUser()
+        {
+            ViewBag.UserList = userMapper.ListAllUsers();
+            return View();
+        }
+
         //查询订单
         public ActionResult SearchOrders()
         {
@@ -49,6 +57,18 @@ namespace duangduangwang.Controllers
 
         }
 
+        //查询用户
+        public ActionResult SearchUsers()
+        {
+            string userId = Request["userId"];  //如果为空则id为""
+            string username = Request["userName"];
+            string member = Request["member"];
+            string phonenumber = Request["phoneNumber"];
+            string[] query = new string[] { userId, username, member, phonenumber };
+            ViewBag.UserList = userMapper.SearchUsers(query);
+            return View("ManageUser");
+        }
+
         //查看某个订单详情
         public ActionResult OrderDetail(int OrderId)
         {
@@ -56,12 +76,6 @@ namespace duangduangwang.Controllers
             ViewBag.Order = orderMapper.getOrderDetail(OrderId);
            
             return View("ManageOrder");
-        }
-
-        //跳转到用户界面/显示所有用户
-        public ActionResult ManageUser()
-        {
-            return View();
         }
 
         //跳转到图书管理界面/显示所有图书
